b444439932794a60ddf38ad7b1f0ed6827ef0815
Jorge Siqueira <jorge.siqueira@gmail.com>
2017-03-28 10:23:09 +0200

    Clean-up

--- XCSTutorialProject1Tests/XCSTutorialProject1Tests.swift
+++ XCSTutorialProject1Tests/XCSTutorialProject1Tests.swift
@@ -30,7 +30,7 @@ class XCSTutorialProject1Tests: XCTestCase {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
-            Thread.sleep(forTimeInterval: 0.1)
+            // Thread.sleep(forTimeInterval: 0.1)
         }
     }
     
